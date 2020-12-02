582b8a1357dbf950b9532c67aa3c39fa6ab52d85
potados99 <potados99@gmail.com>
2020-12-02 11:01:59 +0900

    Change body format.

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,104 @@ export = (app: Application) => {
      context.log.info(`issue commented!: ${context.payload.comment.body}`)
   })
 
-  app.on('pull_request.opened', async (context) => {
-    context.log.info(`pr opened!: ${context.payload}`)
+  app.on('create', async (context) => {
+    if (context.payload.ref_type === 'tag') {
+      context.log.info(`New tag: ${context.payload.ref}`)
+
+      const drafter = new ReleaseDrafter(context);
+      await drafter.draftRelease();
+    }
   });
 
-  app.on('pull_request.reopened', async (context) => {
-    context.log.info(`pr reopened!: ${context.payload}`)
-  });
+}
 
-  app.on('pull_request.closed', async (context) => {
-    context.log.info(`pr reopened!: ${context.payload}`)
-  });
+class ReleaseDrafter {
+  private context: any;
 
+  constructor(context: any) {
+    this.context = context;
+  }
+
+  async draftRelease() {
+    const latestReleaseCommitSha = await this.getLatestReleaseSha();
+
+    const newCommits = await this.getCommitsSinceLastRelease(latestReleaseCommitSha);
+
+    const commitDescriptions = this.createCommitDescriptions(newCommits);
+
+    const releaseBody = this.generateReleaseBody(commitDescriptions);
+
+    await this.pushRelease(releaseBody);
+  }
+
+  private getRepoAndOwner() {
+    const repoName = this.context.payload.repository.name;
+    const ownerName = this.context.payload.repository.owner.login;
+
+    return {repoName, ownerName};
+  }
+
+  private async getLatestReleaseSha() {
+    const {repoName, ownerName} = this.getRepoAndOwner();
+
+    let latestRelease;
+    try {
+      latestRelease = await this.context.octokit.repos.getLatestRelease({
+        owner: ownerName,
+        repo: repoName,
+      });
+    } catch (e) {
+      return null;
+    }
+
+    const latestTagName = latestRelease.data.tag_name;
+
+    const latestTag = await this.context.octokit.git.getRef({
+      owner: ownerName,
+      repo: repoName,
+      ref: `tags/${latestTagName}`
+    });
+
+    return latestTag.data.object.sha;
+  }
+
+  private async getCommitsSinceLastRelease(lastReleaseSha: string) {
+    if (!lastReleaseSha) {
+      return [];
+    }
+
+    const {repoName, ownerName} = this.getRepoAndOwner();
+
+    const comparedResultFromLatestTagWithHead = await this.context.octokit.repos.compareCommits({
+      owner: ownerName,
+      repo: repoName,
+      base: lastReleaseSha,
+      head: 'HEAD'
+    })
+
+    return comparedResultFromLatestTagWithHead.data.commits;
+  }
+
+  private createCommitDescriptions(commits: [any]) {
+    return (commits.length > 0) ?
+        commits.map((c) => `- [\`${c.sha.substring(0, 7)}\`](${c.html_url}) ${c.commit.message}`).join('\n') :
+        'Initial release!';
+  }
+
+  private generateReleaseBody(commitDescriptions: string) {
+    return `## 변경 사항🥳 \n\n${commitDescriptions}`;
+  }
+
+  private async pushRelease(body: string) {
+    const {repoName, ownerName} = this.getRepoAndOwner();
+    const newTagName = this.context.payload.ref;
+
+    await this.context.octokit.repos.createRelease({
+      owner: ownerName,
+      repo: repoName,
+      tag_name: newTagName,
+      name: newTagName,
+      body: body,
+    });
+  }
 }
